89362048ec9d9f197a4622fe6bae3f194c93ba25
RogerEaux <121988408+RogerEaux@users.noreply.github.com>
2023-10-15 14:48:22 -0600

    Add functions to create project and todo containers for main section

--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
 import createContent from './modules/UI';
-import createTodoList from './modules/main-logic';
 
 createContent();
-createTodoList();
--- src/modules/UI.js
+++ src/modules/UI.js
@@ -1,5 +1,6 @@
 import '../style.css';
 import checkmark from '../images/done.svg';
+import createTodoList from './todoList';
 
 const createTop = () => {
   const top = document.createElement('header');
@@ -26,8 +27,48 @@ const createSide = () => {
   return side;
 };
 
+const createTodoContainer = (title, dueDate) => {
+  const todoContainer = document.createElement('div');
+  const taskCompleted = document.createElement('input');
+  const taskTitle = document.createElement('label');
+  const taskDueDate = document.createElement('p');
+
+  todoContainer.classList.add('task-container');
+  todoContainer.setAttribute('id', title);
+  taskCompleted.setAttribute('type', 'checkbox');
+  taskCompleted.setAttribute('name', 'checkbox');
+  taskCompleted.setAttribute('id', title);
+  taskCompleted.classList.add('checkbox-field');
+  taskTitle.setAttribute('for', title);
+  taskTitle.classList.add('checkbox-label');
+  taskTitle.textContent = title;
+  taskDueDate.textContent = dueDate;
+
+  todoContainer.appendChild(taskCompleted);
+  todoContainer.appendChild(taskTitle);
+  todoContainer.appendChild(taskDueDate);
+
+  return todoContainer;
+};
+
 const createMain = () => {
   const main = document.createElement('main');
+  const todoList = createTodoList();
+
+  todoList.projects.forEach((project) => {
+    const projectContainer = document.createElement('div');
+    projectContainer.classList.add('project-container');
+    projectContainer.setAttribute('id', project.name);
+
+    project.todos.forEach((todo) => {
+      const title = todo.title;
+      const dueDate = todo.dueDate;
+      const todoContainer = createTodoContainer(title, dueDate);
+
+      projectContainer.appendChild(todoContainer);
+    });
+    main.appendChild(projectContainer);
+  });
 
   main.classList.add('main');
   main.appendChild(createSide());
